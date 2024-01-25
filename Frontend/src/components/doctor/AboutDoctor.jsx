48f9e79fefd37d9c05a299092ffd1fbc9826a072
Habip Hakkioglu <h.hakkioglu@icloud.com>
2024-01-25 17:14:10 +0100

    doctor page arrangements and new assets

--- Frontend/src/components/doctor/AboutDoctor.jsx
+++ Frontend/src/components/doctor/AboutDoctor.jsx
@@ -1,6 +1,11 @@
 import React, { useEffect } from 'react'
 import useDataCall from '../../hooks/useDataCall'
 
+import user2Icon from '../../assets/user2.png'
+import levelIcon from '../../assets/level.png'
+import starIcon from '../../assets/star.png'
+import commentIcon from '../../assets/comment.png'
+
 const AboutDoctor = ({about, languages}) => {
 
   const {getData} = useDataCall()
@@ -21,6 +26,24 @@ const AboutDoctor = ({about, languages}) => {
         {about}
       </p>
       <button className='text-lg'>Sprachen: Deutsch, {languages.map((lang) => lang, ",")}</button>
+      <div className='flex justify-around items-start'>
+        <div className='flex flex-col w-20 items-center'>
+          <img src={user2Icon} alt="userIcon" width={60}/>
+          <h3>+1000 Patienten</h3>
+        </div>
+        <div className='flex flex-col w-20 items-center'>
+          <img src={levelIcon} alt="levelIcon" width={60}/>
+          <h3>+10 Jahre</h3>
+        </div>
+        <div className='flex flex-col w-20 items-center'>
+          <img src={starIcon} alt="starIcon" width={60}/>
+          <h3>4.6</h3>
+        </div>
+        <div className='flex flex-col w-20 items-center'>
+          <img src={commentIcon} alt="userIcon" width={60}/>
+          <h3>30 Comments</h3>
+        </div>
+      </div>
 
     </div>
   )
